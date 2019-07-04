4b596118fedc14a0c208f726a1806c76ee31225f
otzoy97 <otzoy97@hotmail.com>
2019-07-03 22:14:36 -0600

    Se agregó un iterador para la clase lista

--- src/java/com/estructura/lista/Lista.java
+++ src/java/com/estructura/lista/Lista.java
@@ -5,17 +5,21 @@
  */
 package com.estructura.lista;
 
+import java.util.Iterator;
+
 /**
  * Lista doblemente enlazada genérica
+ *
  * @author otzoy
  * @param <T> Tipo de dato a almacenar en la lista
  */
-public class Lista<T> {
+public class Lista<T> implements Iterable<T> {
     /**
      * Nodo que compone la lista doblemente enlazada
-     * @param <T> 
+     * @param <T>
      */
-    class Nodo<T>{
+    class Nodo<T> {
+
         /**
          * Apuntadores
          */
@@ -24,49 +28,93 @@ public class Lista<T> {
          * Dato que almacena el nodo
          */
         private T dato;
-        Nodo(T dato_){
+
+        Nodo(T dato_) {
             siguiente = null;
             anterior = null;
             dato = dato_;
         }
+
         /**
-         * 
+         *
          * @return recupera el dato almacenado en el nodo
          */
-        public T Dato(){
+        public T Dato() {
             return dato;
         }
+
         /**
          * Modifica el dato almacenado en el nodo
+         *
          * @param dato_ nuevo dato
          */
-        public void Dato(T dato_){
+        public void Dato(T dato_) {
             dato = dato_;
         }
     }
-    
-    private Nodo primero;
-    private Nodo ultimo;
+    /**
+     * Iterador para la clase Lista
+     */
+    class ListaIterador implements Iterator<T> {
+        /**
+         * Apuntador al nodo actual :v
+         */
+        Nodo<T> actual;
+        /**
+         * Constructor por defecto
+         * @param lista
+         */
+        public ListaIterador(Lista<T> lista) {
+            actual = lista.primero;
+        }
+        /**
+         * 
+         * @return <code>true</code> si existe un nodo en la lista, de lo contrario <code>false</code>
+         */
+        @Override
+        public boolean hasNext() {
+            return actual != null;
+        }
+        /**
+         * Retorna el dato que el nodo actual almacena y mueve el puntero del nodo actual al siguiente
+         * @return el dato que el nodo actual almacena
+         */
+        @Override
+        public T next() {
+            T data = actual.Dato();
+            actual = actual.siguiente;
+            return data;
+        }
+    }
+    /**
+     * Apuntadores hacia la cabeza y cola de la lista
+     */
+    Nodo<T> primero, ultimo;
+    /**
+     * Almacena el largo de la lista
+     */
     private int largo;
-    
-    Lista(){
+    /**
+     * Constructor por defecto
+     */
+    public Lista() {
         primero = null;
         ultimo = null;
     }
     /**
-     * Devuelve <code>true</code> si la lista no contiene algún nodo, <code>false</code> si tiene al menos un nodo
-     * @return 
+     * @return Devuelve <code>true</code> si la lista no contiene algún nodo,
+     * <code>false</code> si tiene al menos un nodo
      */
-    public boolean esVacio(){
+    public boolean esVacio() {
         return primero == null;
     }
     /**
      * Agrega un nuevo nodo al final de la lista
-     * @param dato_
+     * @param dato_ que se almacenará al final de lista
      */
-    public void agregarAlFinal(T dato_){
+    public void agregarAlFinal(T dato_) {
         Nodo<T> nuevo = new Nodo<>(dato_);
-        if(!esVacio()){
+        if (!esVacio()) {
             nuevo.anterior = ultimo;
             ultimo.siguiente = nuevo;
             ultimo = nuevo;
@@ -76,18 +124,13 @@ public class Lista<T> {
         }
         largo++;
     }
-//    public void imprimir(){
-//        Nodo<T> temp = primero;
-//        while(temp!=null){
-//            System.out.println(temp.Dato());    
-//            temp = temp.siguiente;
-//        }
-//    }
-//    public static void main(String args[]){
-//        Lista<Integer> a = new Lista();
-//        a.agregarAlFinal(10);
-//        a.agregarAlFinal(9);
-//        a.agregarAlFinal(8);
-//        a.imprimir();
-//    }
+    /**
+     *
+     * @return iterador para la clase lista
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new ListaIterador(this);
+    }
 }
+    
